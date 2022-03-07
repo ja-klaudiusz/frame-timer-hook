e6e2e34617952ec65f3c95b77c50dedcfe14b0fa
Klaudiusz-Marszalek <klaudiusz.w.m@gmail.com>
2022-03-07 20:20:21 +0100

    Prevent unwanted hook refreshing by clock state value

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const timeDefault = {
 };
 
 export const useFrameTimer = (cb, minFps = 1, stopInfo) => {
-  const [clock, setClock] = useState(0);
   const [stop, setStop] = useState(false);
   const [start, setStart] = useState(false);
   const timeRef = useRef(timeDefault);
@@ -18,13 +17,14 @@ export const useFrameTimer = (cb, minFps = 1, stopInfo) => {
   const pauseTimestampRef = useRef(0);
   const pauseTimeRef = useRef(0);
   const counterTimeRef = useRef(0);
+  const clockRef = useRef(0);
   const fpsRef = useRef([]);
   const lastCalledTimeRef = useRef(0);
 
   const actionReset = () => {
     setStop(false);
     setStart(false);
-    setClock(0);
+    clockRef.current = 0;
     timeRef.current = timeDefault;
     timerRef.current = undefined;
     pauseTimestampRef.current = 0;
@@ -73,6 +73,7 @@ export const useFrameTimer = (cb, minFps = 1, stopInfo) => {
   const animate = () => {
     const msTime =
       performance.now() - timestampRef.current - pauseTimeRef.current;
+    const newClock = Math.floor(msTime * (minFps / 1000));
     if (msTime) {
       const ms = Math.floor(msTime);
 
@@ -84,51 +85,48 @@ export const useFrameTimer = (cb, minFps = 1, stopInfo) => {
         d: Math.floor(ms / 1000 / 3600 / 24),
       };
 
-      if (!!stopInfo && !stopInfo[1] && msTime >= stopInfo[0]) {
+      if (!pause && !!stopInfo && !stopInfo[1] && msTime >= stopInfo[0]) {
         delayStop();
         return;
       }
-      setClock(Math.floor(msTime * (minFps / 1000)));
+
+      if (clockRef.current !== newClock) {
+        if (lastCalledTimeRef.current) {
+          const delta = (performance.now() - lastCalledTimeRef.current) / 1000;
+          if (fpsRef.current.length > 5) {
+            fpsRef.current.splice(5);
+          }
+          fpsRef.current.unshift(
+            parseFloat((1 / delta).toFixed(minFps < 1 ? 3 : 0))
+          );
+        }
+        lastCalledTimeRef.current = performance.now();
+        !((!start && timeRef.current.ms > 0) || stop) &&
+          cb({
+            counter: counterTimeRef.current,
+            time: timeRef.current,
+            fps:
+              !start || stop
+                ? 0
+                : fpsRef.current.length === 6
+                ? Math.max.apply(Math, fpsRef.current.slice(0, 3))
+                : 0,
+            ...cbData,
+          });
+
+        if (start) {
+          if (stopInfo[1] && stopInfo[0] === counterTimeRef.current) {
+            delayStop();
+            return;
+          }
+          counterTimeRef.current++;
+        }
+      }
+      clockRef.current = newClock;
 
       timerRef.current = requestAnimationFrame(animate);
     }
   };
-
-  useLayoutEffect(() => {
-    if (lastCalledTimeRef.current) {
-      const delta = (performance.now() - lastCalledTimeRef.current) / 1000;
-      if (fpsRef.current.length > 5) {
-        fpsRef.current.splice(5);
-      }
-      fpsRef.current.unshift(
-        parseFloat((1 / delta).toFixed(minFps < 1 ? 3 : 0))
-      );
-    }
-    lastCalledTimeRef.current = performance.now();
-    !((!start && timeRef.current.ms > 0) || stop) &&
-      cb({
-        counter: counterTimeRef.current,
-        time: timeRef.current,
-        fps:
-          !start || stop
-            ? 0
-            : fpsRef.current.length === 6
-            ? Math.max.apply(Math, fpsRef.current.slice(0, 3))
-            : 0,
-        ...cbData,
-      });
-
-    if (start) {
-      if (stopInfo[1] && stopInfo[0] === counterTimeRef.current) {
-        delayStop();
-        return;
-      }
-      counterTimeRef.current++;
-    }
-    if (stop) {
-      counterTimeRef.current = 0;
-    }
-  }, [clock, start, stop]);
 
   useLayoutEffect(() => {
     if (start) {
@@ -158,6 +156,12 @@ export const useFrameTimer = (cb, minFps = 1, stopInfo) => {
     if (stop) {
       cancelAnimationFrame(timerRef.current);
       actionReset();
+      cb({
+        time: timeDefault,
+        counter: 0,
+        fps: 0,
+        ...cbData,
+      });
     }
     return () => cancelAnimationFrame(timerRef.current);
   }, [start, stop]);
